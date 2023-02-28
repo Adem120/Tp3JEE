b244bc7132f9d31dd113fca4f63d52cba2578220
Adem120 <adem99538@gmail.com>
2023-02-28 21:55:34 +0100

    first commit

---
+++ src/main/java/web/ControleurServlet.java
@@ -0,0 +1,35 @@
+package web;
+
+import metier.ImetierCatalogue;
+import metier.MetierImpl;
+import metier.Produit;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet (name="controlleur",value= "/controleur")
+public class ControleurServlet extends HttpServlet {
+    private ImetierCatalogue metier;
+    @Override
+    public void init() throws ServletException {
+        metier=new MetierImpl();
+    }
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String mc=request.getParameter("motCle");
+        ProduitModele mod = new ProduitModele();
+        mod.setMotCle(mc);
+        List<Produit> prods = metier.getProduitsParMotCle(mc);
+        mod.setProduits(prods);
+        request.setAttribute("modele", mod);
+        request.getRequestDispatcher("ProduitsView.jsp").forward(request,response);
+    }
+}
